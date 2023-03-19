ffa4256e20f5cc6240c28632df8846b31217f081
Philipp Viereck <philipp@mailbean.io>
2023-03-19 18:15:34 +0100

    fix broken demonstration of issue

--- src/routes/BUG/+page.ts
+++ src/routes/BUG/+page.ts
@@ -1,1 +1,2 @@
-export const ssr = false // Fix Yjs complaining: "Yjs was already imported. This breaks constructor checks and will lead to issues! - https://github.com/yjs/yjs/issues/438"
+// Fix Yjs complaining: "Yjs was already imported. This breaks constructor checks and will lead to issues! - https://github.com/yjs/yjs/issues/438"
+// export const ssr = false; --> this fixes Yjs complaining, but breaks the showcase
