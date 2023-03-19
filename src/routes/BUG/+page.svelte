fdd2d7790551495a0fbb38810d57bf8da74f2e5e
Philipp Viereck <philipp@mailbean.io>
2023-03-19 14:25:40 +0100

    bug reprod

---
+++ src/routes/BUG/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { browser } from '$app/environment'
+	import { onDestroy } from 'svelte'
+	import Y from 'yjs'
+	import { supabaseClient } from '$lib/supabaseClient'
+	import SupabaseProvider from '$lib/y-supabase-custom'
+
+	const ydoc = new Y.Doc()
+
+	const provider = browser
+		? new SupabaseProvider(ydoc, supabaseClient, {
+				channel: 'demo',
+				id: 'demo1',
+				tableName: 'notes',
+				columnName: 'ydoc'
+		  })
+		: null
+
+	onDestroy(() => {
+		provider?.destroy()
+	})
+</script>
